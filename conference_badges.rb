4b30b7d71075adc3c41bff4650357d3afbba5029
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-12 23:00:40 +0000

    Complete badges_and_schedules lab

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  return  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  result = array.map{|string|badge_maker(string)}
+end
+
+def assign_rooms(array)
+  result = []
+  array.each_with_index{|name, room_num|result << "Hello, #{name}! You'll be assigned to room #{room_num+1}!"}
+  result
+end
+
+def printer(array)
+  badges = batch_badge_creator(array)
+  room_assignments = assign_rooms(array)
+  
+  badges.each{|line|puts line}
+  room_assignments.each{|line|puts line}
+end
