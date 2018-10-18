a08a54c17189679ebfa97b9100af6025190fe07c
Eizik Gottesfeld <ezkmonsey@gmail.com>
2018-10-18 01:15:15 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}." 
+
+
+def batch_badge_creator(array)
+  message=[]
+  for i in 0..array.length-1 do
+    message << "Hello, my name is #{array[i]}."
+  end
+  message
+end 
+
+def assign_rooms(array)
+  message=[]
+  for i in 0..array.length-1 do
+    message << "Hello, #{array[i]}! You'll be assigned to room #{i+1}!"
+  end
+  message
+end 
+
+def printer
+  
+  
+end 
